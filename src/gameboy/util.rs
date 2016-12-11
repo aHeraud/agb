434056fbe2f826e7f8db16381f800c6e96143886
Achille Heraud <achille@heraud.xyz>
2016-12-10 20:03:31 -0600

    Removed file io stuff.

--- src/gameboy/util.rs
+++ src/gameboy/util.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
-use std::io::{Read, Error};
-use std::path::Path;
+//use std::fs::File;
+//use std::io::{Read, Error};
+//use std::path::Path;
 use std::num::Wrapping;
 
 pub fn wrapping_add(r1: u16, r2: u16) -> u16 {
@@ -11,14 +11,13 @@ pub fn wrapping_sub(r1: u16, r2: u16) -> u16 {
 	(Wrapping(r1) - Wrapping(r2)).0
 }
 
+/*
 pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Box<[u8]>, Error> {
 	let mut file = try!(File::open(path));
 	let mut buffer = Vec::new();
 	let result = file.read_to_end(&mut buffer);
-	//try!(file.read_to_end(&mut buffer));
-	//Ok(buffer.into_boxed_slice())
 	match result {
 		Ok(_) => Ok(buffer.into_boxed_slice()),
 		Err(err) => Err(err),
 	}
-}
+}*/
