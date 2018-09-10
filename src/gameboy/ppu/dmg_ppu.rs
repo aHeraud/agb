aHeraud/agb

commit cb43908328bc81a01e381b7a30418da920dea574

Removed drain_filter feature

src/gameboy/ppu/dmg_ppu.rs
@@ -210,12 +210,10 @@ impl DmgPpu {
 				tile_number: data[2],
 				attributes: data[3]
 			}
-		}).collect();
-
-		// Remove sprites that don't appear on the current line
-		sprites.drain_filter(|sprite| {
+		}).filter(|sprite| {
+			// Remove sprites that don't appear on the current line
 			sprite.y_pos() > line || sprite.y_pos() + height < line
-		});
+		}).collect::<Vec<Sprite>>();
 
 		// In DMG mode, sprites are prioritized based on x coordinate. (lowest x coordinate = highest priority)
 		sprites.sort_by_key(|sprite| sprite.x);

commit 7ef9e49c3f408d5d0a2c68a0336e3cd22046f315

Fixed the bug introduced by cb43908328bc81a01e381b7a30418da920dea574 that caused sprites to not appear and the emulator to crash randomly.

src/gameboy/ppu/dmg_ppu.rs
@@ -203,16 +203,20 @@ impl DmgPpu {
 
 		// There is an attribute table for 40 sprits in oam,
 		// each sprite attribute table entry is 4 bytes long
-		let mut sprites: Vec<Sprite> = self.oam.chunks(4).map(|data| {
-			Sprite {
+		let mut sprites: Vec<Sprite> = self.oam.chunks(4).filter_map(|data| {
+			let sprite = Sprite {
 				y: data[0],
 				x: data[1],
 				tile_number: data[2],
 				attributes: data[3]
+			};
+
+			if sprite.y_pos() > line || sprite.y_pos() + height < line {
+				None
 			}
-		}).filter(|sprite| {
-			// Remove sprites that don't appear on the current line
-			sprite.y_pos() > line || sprite.y_pos() + height < line
+			else {
+				Some(sprite)
+			}
 		}).collect::<Vec<Sprite>>();
 
 		// In DMG mode, sprites are prioritized based on x coordinate. (lowest x coordinate = highest priority)